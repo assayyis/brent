e1e48a4219e2f5dc17724b93a0684b85da290f4c
mohibnu <assayyis2@gmail.com>
2024-01-07 09:57:54 +0700

    Initial commit

---
+++ src/main/java/com/ibnu/brent/BrentApplication.java
@@ -0,0 +1,13 @@
+package com.ibnu.brent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibnu/brent/BrentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibnu.brent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
